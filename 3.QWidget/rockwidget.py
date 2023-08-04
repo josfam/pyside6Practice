f84482a9065e59f52070c70ce3b8bafd10524a6d
josfam <josephamukun@tutanota.com>
2023-08-04 23:21:06 +0300

    Add buttons in  horizontal layout with QHBoxLayout

--- 3.QWidget/rockwidget.py
+++ 3.QWidget/rockwidget.py
@@ -1,14 +1,18 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
+
 class RockWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('RockWidget')
-        button1 = QPushButton('Button1')
-        
+        self.setWindowTitle("RockWidget")
+
+        button1 = QPushButton("Button1")
+        button2 = QPushButton("Button2")
+
         # a horizontal layout for placing components
-        button_layout = QHBoxLayout()
+        button_layout = QHBoxLayout()  # use QVBoxLayout for vertical layouts
         button_layout.addWidget(button1)
+        button_layout.addWidget(button2)
 
         # use the layout to lay out various components
         self.setLayout(button_layout)
