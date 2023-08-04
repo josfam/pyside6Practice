4b6c3e8c1763d57942c03529bc0318129cf35973
josfam <josephamukun@tutanota.com>
2023-08-04 18:53:23 +0300

    Add signals/slots example with a checkable button

--- 2.signals_and_slots/main.py
+++ 2.signals_and_slots/main.py
@@ -2,12 +2,15 @@ from PySide6.QtWidgets import QApplication, QPushButton
 
 
 # the slot that responds when something happens
-def button_clicked():
-    print("You clicked the button, didn't you!")
+def button_clicked(data):
+    print("You clicked the button, didn't you! checked : ", data)
 
 
 app = QApplication()
 button = QPushButton("Press Me")
+
+# make the button checkable such that it toggles between checked and unchecked
+button.setCheckable(True)
 
 # Use the emitted `clicked` signal of the QPushButton to wire the slot
 button.clicked.connect(button_clicked)
