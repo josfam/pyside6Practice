2dc1229546e811773cebfcd51c4debb5b2220799
josfam <josephamukun@tutanota.com>
2023-08-04 19:22:50 +0300

    Add the slider signal and slot example

--- 2.signals_and_slots/main.py
+++ 2.signals_and_slots/main.py
@@ -1,19 +1,22 @@
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import QApplication, QSlider
+from PySide6.QtCore import Qt
 
 
-# the slot that responds when something happens
-def button_clicked(data):
-    print("You clicked the button, didn't you! checked : ", data)
+# the slot to respond to the slider
+def respond_to_slider(data):
+    print("slider moved to : ", data)
 
 
 app = QApplication()
-button = QPushButton("Press Me")
 
-# make the button checkable such that it toggles between checked and unchecked
-button.setCheckable(True)
+# create the slider
+slider = QSlider(Qt.Horizontal)
+slider.setMinimum(0)
+slider.setMaximum(100)
+slider.setValue(25)
 
-# Use the emitted `clicked` signal of the QPushButton to wire the slot
-button.clicked.connect(button_clicked)
-
-button.show()
+# use the `valueChanged` signal emitted from the slider to
+# connect to the slot
+slider.valueChanged.connect(respond_to_slider)
+slider.show()
 app.exec()
