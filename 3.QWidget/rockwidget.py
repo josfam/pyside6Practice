2f7d2810934aef4e62808ea41201647f5a7a3da8
josfam <josephamukun@tutanota.com>
2023-08-04 23:38:34 +0300

    Add signals and slots for horizontal buttons

--- 3.QWidget/rockwidget.py
+++ 3.QWidget/rockwidget.py
@@ -7,7 +7,9 @@ class RockWidget(QWidget):
         self.setWindowTitle("RockWidget")
 
         button1 = QPushButton("Button1")
+        button1.clicked.connect(self.button1_clicked)
         button2 = QPushButton("Button2")
+        button2.clicked.connect(self.button2_clicked)
 
         # a horizontal layout for placing components
         button_layout = QHBoxLayout()  # use QVBoxLayout for vertical layouts
@@ -16,3 +18,10 @@ class RockWidget(QWidget):
 
         # use the layout to lay out various components
         self.setLayout(button_layout)
+
+    # set up the slots
+    def button1_clicked(self):
+        print("Button 1 clicked")
+
+    def button2_clicked(self):
+        print("Button2 clicked")
