2ab521b0d5438b9c2c4a64eecc8a0bd6d9d54945
josfam <josephamukun@tutanota.com>
2023-08-04 12:01:13 +0300

    Add code for displaying a button

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # responsible for processing commandline arguments
 import sys
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = QMainWindow()
+window.setWindowTitle("Our first MainWindow App!")
+
+# create a button
+button = QPushButton()
+button.setText("Press Me")
+
+window.setCentralWidget(button)
+
 window.show()
 
 # start the event loop
