272b2776e58d4904c2d6a259a51800030eeb055b
josfam <josephamukun@tutanota.com>
2023-08-13 12:04:28 +0300

    Add an `Edit` menu and its actions

--- 4.QMainWindow/main_window.py
+++ 4.QMainWindow/main_window.py
@@ -6,12 +6,20 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.app = app  # declare an app member
         self.setWindowTitle("Custom MainWindow")
-    
+
         # setup menu bar and menu
         menu_bar = self.menuBar()
         file_menu = menu_bar.addMenu("&File")
         quit_action = file_menu.addAction("Quit")
         quit_action.triggered.connect(self.quit_app)
-    
+
+        # add an edit menu and its actions
+        edit_menu = menu_bar.addMenu("&Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
+
     def quit_app(self):
         self.app.quit()
